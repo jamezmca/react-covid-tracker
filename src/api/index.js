ca64eb45d939bb9cb7129de3cb04302b92a02cb9
James McArthur <jamesmca@live.com>
2021-04-16 12:09:30 -0700

    chart fetch api and general fetch api functional and chart.js line chart
    2x useEffect running at different times and displaying content depending on whether or not fetch api has loaded content. Confirmed cases, recovered and deaths. And then plotting it using chart.js v2 Line graph

--- src/api/index.js
+++ src/api/index.js
@@ -22,3 +22,17 @@ export async function fetchData() {
       return modifiedData
       works because if the name is the value and the key, auto inputs as both
       */
+
+export async function fetchDailyData() {
+    try {
+        const { data } = await axios.get(`${url}/daily`)
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate
+        }))
+        return modifiedData
+    } catch (err) {
+        console.log(err)
+    }
+}
--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api'
+import { Line, Bar } from 'react-chartjs-2'
+import styles from './Chart.module.css'
 
 const Chart = () => {
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        async function fetchAPI() {
+            const response = await fetchDailyData();
+            setDailyData(response)
+        }
+        fetchAPI()
+    }, [setDailyData])
+
+ 
+
+    const lineChart = (dailyData.length ? <Line data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [{
+            data: dailyData.map(({ confirmed }) => confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true
+        },
+        {
+            data: dailyData.map(({ deaths }) => deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true
+        }]
+    }} /> : null)
+
     return (
-        <div>
-            
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
